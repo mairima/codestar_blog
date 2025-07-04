760e96bc095a6ebd7b7344e763913a57deb732e4
mairima <aissatou.mairi@gmail.com>
2025-07-04 06:10:21 +0200

    fix STATIC_ROOT for heroku collectstatic

--- codestar/settings.py
+++ codestar/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = []
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_DIRS = [] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
