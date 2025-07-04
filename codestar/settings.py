8bf20d656effcd0f4018b96307ccfdad58599af5
mairima <aissatou.mairi@gmail.com>
2025-07-04 04:54:05 +0200

    make it better

--- codestar/settings.py
+++ codestar/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
