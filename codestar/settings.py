bc07d8bd2dd6309d8446d8cc2b6b91f9c7c00e4f
mairima <aissatou.mairi@gmail.com>
2025-07-05 05:15:53 +0200

    sync migrations and clean up

--- codestar/settings.py
+++ codestar/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 
 CSRF_TRUSTED_ORIGINS = [
