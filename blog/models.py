8a602c0782e5cedbea9817dbeaf98e99cb576309
mairima <aissatou.mairi@gmail.com>
2025-07-06 05:15:56 +0200

    Add comment in models

--- blog/models.py
+++ blog/models.py
@@ -16,29 +16,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-
-    # models relationship
-class Event(models.Model):
-    event_name = models.CharField(max_length=200, unique=True)
-    location = models.CharField(max_length=200)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.event_name
-
-
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
-    )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
-
-    def __str__(self):
-        return f"Ticket for {self.ticket_holder}"
+   
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
