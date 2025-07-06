cbbada7d4e681eee760ec555f93e68659d6a2f9f
mairima <aissatou.mairi@gmail.com>
2025-07-06 09:49:01 +0200

    Add import comments

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-   
+    
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -31,9 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
-   class Meta:
+
+    class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}" 
+        return f"Comment {self.body} by {self.author}"
