5585126e7dccdae91878184f03b8693a0b57b18f
mairima <aissatou.mairi@gmail.com>
2025-07-07 02:43:33 +0200

    Update allowedhost on settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme-in-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com',
-                 '127.0.0.1',]
+ALLOWED_HOSTS = ['codestarblog-ma-1e43141bb4e8.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
