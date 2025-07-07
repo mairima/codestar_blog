4d03323d5befee62c49631f5c5bf802597091926
mairima <aissatou.mairi@gmail.com>
2025-07-07 03:08:21 +0200

    Update blog codestar file

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import PostList
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', PostList.as_view(), name='home'),
     path("", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls),
 ]
