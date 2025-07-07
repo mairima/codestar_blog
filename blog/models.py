b07510e125f670067391ad5c4d0fa771d9e91e55
mairima <aissatou.mairi@gmail.com>
2025-07-07 03:03:03 +0200

    Update blog migration file

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
-STATUS = (
-    (0, "Draft"),
-    (1, "Published"),
-)
+from django.contrib.auth.models import User # Import models to connect
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -22,7 +21,8 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-    
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
@@ -31,6 +31,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField()
 
     class Meta:
         ordering = ["created_on"]
