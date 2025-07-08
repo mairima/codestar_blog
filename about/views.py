75d3e9c2afe5d00711421f98e91fe40730952426
mairima <aissatou.mairi@gmail.com>
2025-07-08 08:29:06 +0200

    added authentication

--- about/views.py
+++ about/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request, 'about/about.html')
+    context = {
+        "about": {
+            "title": "About Me",
+            "content": "<p>Hi there! I'm learning Django.</p>",
+            "updated_on": "2025-07-08",
+        }
+    }
+    return render(request, 'about/about.html', context)
